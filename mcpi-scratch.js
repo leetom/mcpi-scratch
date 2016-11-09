bcc9dd1f256ec1873cdfb69772e5c3f6a79e2ae5
leetom <leetom1991@163.com>
2016-11-09 19:16:00 +0800

    add new blocks

--- mcpi-scratch.js
+++ mcpi-scratch.js
@@ -6,7 +6,7 @@
     var server_host = "http://mc.codepku.com";
     var server_port = 4715;
 
-    var request_timeout = 2000;
+    var request_timeout = 4000;
 
     var request_frequency = 4; //每秒限制5次请求，防止服务器端负载过高。
 
@@ -140,10 +140,10 @@
         }); // nb: GET is including the javascript callback. Do I need this for one-way call?
     };
 
-    ext.setLine = function(x1, z1, x2, z2, y, blockType, blockData) {
+    ext.setLine = function(x1, y1, z1, x2, y2, z2, blockType, blockData) {
         if(!ext.checkReady() || ext.checkOverclock()) return;
-        var cmdUrl = server_url + "/setLine/" + x1 + "/" + z1 + "/" 
-            + x2 + "/" + z2 + "/" + y + "/" + blockType + "/" + blockData;
+        var cmdUrl = server_url + "/setLine/" + x1 + "/" + y1 + "/" + z1 + "/"
+            + x2 + "/" + y2 + "/" + z2 + "/" + blockType + "/" + blockData;
         $.ajax({
             timeout: request_timeout,
             type: "POST",
@@ -158,10 +158,11 @@
         }); // nb: GET is including the javascript callback. Do I need this for one-way call?
     };
 
-    ext.setCircle = function(x, z, r, y, blockType, blockData) {
+    ext.setCircle = function(x, y, z, r, blockType, blockData, horizontal = false) {
         if(!ext.checkReady() || ext.checkOverclock()) return;
-        var cmdUrl = server_url + "/setCircle/" + x + "/" + z + "/" 
-            + r + "/" + y + "/" + blockType + "/" + blockData;
+        var cmd = horizontal ? "/setHCircle/" : "/setCircle/";
+        var cmdUrl = server_url + cmd + x + "/" + y + "/" 
+            + z + "/" + r + "/" + blockType + "/" + blockData;
         $.ajax({
             timeout: request_timeout,
             type: "POST",
@@ -175,6 +176,48 @@
             }
         }); // nb: GET is including the javascript callback. Do I need this for one-way call?
     };
+
+    ext.setHCircle = function(x, y, z, r, blockType, blockData) {
+       ext.setCircle(x, y, z, r, blockType, blockData, true);
+    };
+
+    ext.setSphere = function(x, y, z, r, blockType, blockData) {
+        if(!ext.checkReady() || ext.checkOverclock()) return;
+
+        var cmdUrl = server_url + "/setSphere/" + x + "/" + y + "/" 
+            + z + "/" + r + "/" + blockType + "/" + blockData;
+        $.ajax({
+            timeout: request_timeout,
+            type: "POST",
+            url: cmdUrl,
+            //dataType: "jsonp", // hack for the not origin problem - replace with CORS based solution
+            success: function(data) {
+                console.log("setSphere success");
+            },
+            error: function(jqxhr, textStatus, error) { // have to change this coz jasonp parse error
+                console.log("Error setSphere: ", error);
+            }
+        }); // nb: GET is including the javascript callback. Do I need this for one-way call?
+    };
+
+    ext.setText = function(str, x, y, z, blockType, blockData){
+        if(!ext.checkReady() || ext.checkOverclock()) return;
+
+        var cmdUrl = server_url + "/setText/" + str + "/" + x + "/" + y + "/" 
+            + z + "/" + blockType + "/" + blockData;
+        $.ajax({
+            timeout: request_timeout,
+            type: "POST",
+            url: cmdUrl,
+            //dataType: "jsonp", // hack for the not origin problem - replace with CORS based solution
+            success: function(data) {
+                console.log("setText success");
+            },
+            error: function(jqxhr, textStatus, error) { // have to change this coz jasonp parse error
+                console.log("Error setText: ", error);
+            }
+        });
+    }
 
     // get one coord (x, y, or z) for playerPos
     ext.getPlayerPos = function(posCoord, callback) {
@@ -318,8 +361,11 @@
             [" ", "set Player pos to x:%n y:%n z:%n", "setPlayerPos", 0, 0, 0],
             [" ", "set block pos x:%n y:%n z:%n to type %n data %n %m.blockPos", "setBlock", 0, 0, 0, 1, -1],
             [" ", "set blocks pos x1:%n y1:%n z1:%n to x2:%n y2:%n z2:%n to type %n data %n", "setBlocks", 0, 0, 0, 0, 0, 0, 1, -1],
-            [" ", "set line pos x1:%n z1:%n to x2:%n z2:%n height y:%n to type %n data %n", "setLine", 0, 0, 0, 0, 0, 1, -1],
-            [" ", "set circle center x1:%n z1:%n radius r:%n at height y:%n to type %n data %n", "setCircle", 0, 0, 0, 0, 0, 1, -1],
+            [" ", "set line from x1:%n y1:%n z1:%n to x2:%n y2:%n z2:%n to type %n data %n", "setLine", 0, 0, 0, 0, 0, 0, 1, -1],
+            [" ", "set circle center x1:%n y1:%n z1:%n radius r:%n to type %n data %n", "setCircle", 0, 0, 0, 0, 1, -1],
+            [" ", "set horizontal circle center x1:%n y1:%n z1:%n radius r:%n to type %n data %n", "setHCircle", 0, 0, 0, 0, 1, -1],
+            [" ", "set sphere center at x1:%n y1:%n z1:%n radius r:%n to type %n data %n", "setSphere", 0, 0, 0, 0, 1, -1],
+            [" ", "set text %s at x1:%n y1:%n z1:%n with type %n data %n", "setText", "Hello", 0, 0, 0, 1, -1],
             ["R", "get player pos %m.pos", "getPlayerPos", 'x'],
             ["R", "get block pos x:%n y:%n z:%n %m.blockPos", "getBlock", 0, 0, 0],
             ["h", "when blockHit", 'whenBlockHit'],
